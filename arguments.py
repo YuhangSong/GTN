8373e8f642d24144765c5ff4373dce6218e26a98
s <YuhangSong2017@gmail.com>
2017-10-20 22:21:38 +0800

    test ewc works?

--- arguments.py
+++ arguments.py
@@ -55,8 +55,8 @@ def get_args():
                         help='value loss coefficient (default: 0.5)')
     parser.add_argument('--seed', type=int, default=1,
                         help='random seed (default: 1)')
-    parser.add_argument('--num-processes', type=int, default=2,
-                        help='how many training CPU processes to use (default: 16)')
+    parser.add_argument('--num-processes', type=int, default=16,
+                        help='how many training CPU processes to use')
     parser.add_argument('--num-steps', type=int, default=5,
                         help='number of forward steps in A2C (default: 5)')
     parser.add_argument('--ppo-epoch', type=int, default=4,
